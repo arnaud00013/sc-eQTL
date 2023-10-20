3f9cc8918797bfae22ff77ea6b21fb2b73402ec8
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2023-10-20 17:28:47 -0400

    Update submit_Count_nb_breakpoints.sh

--- III_Genotype_analysis/submit_Count_nb_breakpoints.sh
+++ III_Genotype_analysis/submit_Count_nb_breakpoints.sh
@@ -7,4 +7,4 @@
 #SBATCH --time=03-00:00            # time (DD-HH:MM)
 
 #Run error-correcting HMM
-/home/p1211536/Tensorflow_mod/bin/python3.9 generate_dist_cell_to_lineage_uncorrected_gen.py /home/p1211536/scratch/NoFastp_Yeast/test_param_bbqtl_hmm_on_scrna_seq /home/p1211536/scratch/NoFastp_Yeast/test_param_bbqtl_hmm_on_scrna_seq /home/p1211536/scratch/NoFastp_Yeast/test_param_bbqtl_hmm_on_scrna_seq 16 500
+python generate_dist_cell_to_lineage_uncorrected_gen.py /home/p1211536/scratch/NoFastp_Yeast/test_param_bbqtl_hmm_on_scrna_seq /home/p1211536/scratch/NoFastp_Yeast/test_param_bbqtl_hmm_on_scrna_seq /home/p1211536/scratch/NoFastp_Yeast/test_param_bbqtl_hmm_on_scrna_seq 16 500
