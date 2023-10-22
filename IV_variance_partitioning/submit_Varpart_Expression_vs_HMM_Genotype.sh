451c78a02b409012f3b90adbc6f9b13736a837c1
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2023-10-22 18:19:56 -0400

    Update submit_Varpart_Expression_vs_HMM_Genotype.sh

--- IV_variance_partitioning/submit_Varpart_Expression_vs_HMM_Genotype.sh
+++ IV_variance_partitioning/submit_Varpart_Expression_vs_HMM_Genotype.sh
@@ -5,4 +5,4 @@
 #SBATCH --cpus-per-task=1         # CPU cores per MPI process
 #SBATCH --mem=100G                # memory per node
 #SBATCH --time=7-00:00            # time (DD-HH:MM)
-/home/p1211536/Tensorflow_mod/bin/python3.9 HMM_based_Expression_heritability.py
+python HMM_based_Expression_heritability.py $the_ind_partition $nb_expression_pcs_partitions $nb_expression_PCs $workspace $cellranger_outs_folder $Nb_cpus
