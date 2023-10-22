3b0efd085d8c33b48db664ac374afc3a6487f64b
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2023-10-22 17:40:10 -0400

    Update submit_Varpart_Pheno_vs_Genotype_and_Expression.sh

--- IV_variance_partitioning/submit_Varpart_Pheno_vs_Genotype_and_Expression.sh
+++ IV_variance_partitioning/submit_Varpart_Pheno_vs_Genotype_and_Expression.sh
@@ -2,4 +2,4 @@
 #INSERT header
 
 #Run script
-python REML_Varpart_Phenotype_vs_Genotype_and_Expression.py
+python REML_Varpart_Phenotype_vs_Genotype_and_Expression.py $workspace $cellranger_outs_folder $Nb_cpus
