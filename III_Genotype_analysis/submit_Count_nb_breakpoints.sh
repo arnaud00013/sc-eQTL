cc524aab75548f76a7a2ca29520ab23b8ace5039
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2023-10-22 17:47:07 -0400

    Update submit_Count_nb_breakpoints.sh

--- III_Genotype_analysis/submit_Count_nb_breakpoints.sh
+++ III_Genotype_analysis/submit_Count_nb_breakpoints.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#SBATCH --account=def-anguyen
-#SBATCH --nodes=1                 # Number of nodes
-#SBATCH --ntasks=16               # Number of MPI process
-#SBATCH --cpus-per-task=1         # CPU cores per MPI process
-#SBATCH --mem=200G                # memory per node
-#SBATCH --time=03-00:00            # time (DD-HH:MM)
+#HEADER
 
 #Run error-correcting HMM
 python Count_nb_breakpoints_per_HMM_imputations.py $workspace $cellranger_outs_folder $Nb_cpus
