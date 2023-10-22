bc49170405ba382743aabad094075ffdc4050a85
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2023-10-22 17:56:16 -0400

    Update submit_generate_pca_expression_partitions.sh

--- IV_variance_partitioning/submit_generate_pca_expression_partitions.sh
+++ IV_variance_partitioning/submit_generate_pca_expression_partitions.sh
@@ -2,4 +2,4 @@
 #HEADER
 
 #Run script
-python generate_pca_expression_partitions.py $nb_expression_pcs_partitions $workspace_path $cellranger_outs_folder $nb_cpus $nb_cells
+python generate_pca_expression_partitions.py $nb_expression_pcs_partitions $workspace $cellranger_outs_folder $nb_cpus $nb_cells
