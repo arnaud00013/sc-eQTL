5f905bc4975e76e59890bfad10584fa2d5d06338
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2023-10-22 17:42:32 -0400

    Update submit_Count_nb_breakpoints.sh

--- III_Genotype_analysis/submit_Count_nb_breakpoints.sh
+++ III_Genotype_analysis/submit_Count_nb_breakpoints.sh
@@ -8,3 +8,4 @@
 
 #Run error-correcting HMM
 python Count_nb_breakpoints_per_HMM_imputations.py $workspace $cellranger_outs_folder $Nb_cpus
+python Count_nb_breakpoints_per_reflineage.py $workspace $cellranger_outs_folder $Nb_cpus
