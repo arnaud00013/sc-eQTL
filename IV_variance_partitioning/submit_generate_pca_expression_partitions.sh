caa18cf329f764b4bda08d9a1244737997a15142
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2023-10-22 17:53:50 -0400

    Update submit_generate_pca_expression_partitions.sh

--- IV_variance_partitioning/submit_generate_pca_expression_partitions.sh
+++ IV_variance_partitioning/submit_generate_pca_expression_partitions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#INSERT header
+#HEADER
 
 #Run script
-/home/p1211536/Tensorflow_mod/bin/python3.9 generate_pca_expression_partitions.py $nb_expression_pcs_partitions $workspace_path $cellranger_outs_folder $nb_cpus $nb_cells
+python generate_pca_expression_partitions.py $nb_expression_pcs_partitions $workspace_path $cellranger_outs_folder $nb_cpus $nb_cells
