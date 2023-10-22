2e1dfd54278cf44410e977fede9cac9838263325
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2023-10-22 17:48:00 -0400

    Update submit_get_uncorrected_gen_dist_mtx.sh

--- III_Genotype_analysis/submit_get_uncorrected_gen_dist_mtx.sh
+++ III_Genotype_analysis/submit_get_uncorrected_gen_dist_mtx.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#SBATCH --account=def-anguyen
-#SBATCH --nodes=1                 # Number of nodes
-#SBATCH --ntasks=16               # Number of MPI process
-#SBATCH --cpus-per-task=1         # CPU cores per MPI process
-#SBATCH --mem=200G                # memory per node
-#SBATCH --time=03-00:00            # time (DD-HH:MM)
+#HEADER
 
 #Run error-correcting HMM
 python generate_dist_cell_to_lineage_uncorrected_gen.py $workspace $cellranger_outs_folder $Nb_cpus $Number_of_subsampes_for_lineage_assignment
