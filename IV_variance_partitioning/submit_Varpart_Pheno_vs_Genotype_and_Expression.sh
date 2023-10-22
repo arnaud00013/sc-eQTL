e05a08d0734afa45eddd341b2df81f04fe39c5bd
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2023-10-22 17:36:04 -0400

    Update submit_Varpart_Pheno_vs_Genotype_and_Expression.sh

--- IV_variance_partitioning/submit_Varpart_Pheno_vs_Genotype_and_Expression.sh
+++ IV_variance_partitioning/submit_Varpart_Pheno_vs_Genotype_and_Expression.sh
@@ -2,4 +2,4 @@
 #INSERT header
 
 #Run script
-/home/p1211536/Tensorflow_mod/bin/python3.9 REML_Varpart_Phenotype_vs_Genotype_and_Expression.py
+python REML_Varpart_Phenotype_vs_Genotype_and_Expression.py
