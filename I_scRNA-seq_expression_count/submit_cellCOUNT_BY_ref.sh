1e4d954dc4b5a9bf8a26891913740aa0b1bab38d
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2023-10-22 17:52:05 -0400

    Update submit_cellCOUNT_BY_ref.sh

--- I_scRNA-seq_expression_count/submit_cellCOUNT_BY_ref.sh
+++ I_scRNA-seq_expression_count/submit_cellCOUNT_BY_ref.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#header
+#HEADER
 
 cellranger count --id=map2BY_ref --transcriptome=$BY_reference --fastqs=$Path_illumina_reads --sample=$BBQscRNA_1 --localcores=$Nb_cpu --localmem=$memory_allocated
